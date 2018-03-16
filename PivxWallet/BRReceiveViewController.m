7782f522ec3da1e6c9b75dbb391a6525e45037b9
Sujal Bandhara <sujal.bandhara@bypt.in>
2018-03-16 16:08:21 +0530

    Added Buy PIVX with Cash button
    Added Buy PIVX with Cash button in receive PIVX screen.

--- PivxWallet/BRReceiveViewController.m
+++ PivxWallet/BRReceiveViewController.m
@@ -44,6 +44,16 @@
 //#define QR_IMAGE_FILE [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES).lastObject\
 //                       stringByAppendingPathComponent:@"qr.png"]
 
+
+//WallOfCoins
+#import "WOCBuyDashStep1ViewController.h"
+#import "WOCBuyingInstructionsViewController.h"
+#import "WOCBuyingSummaryViewController.h"
+#import "WOCConstants.h"
+#import "MBProgressHUD.h"
+#import "BRAppDelegate.h"
+#import "APIManager.h"
+
 @interface BRReceiveViewController ()
 
 @property (nonatomic, strong) UIImage *qrImage;
@@ -503,6 +513,89 @@ presentingController:(UIViewController *)presenting sourceController:(UIViewCont
 - (id<UIViewControllerAnimatedTransitioning>)animationControllerForDismissedController:(UIViewController *)dismissed
 {
     return self;
+}
+
+// Added New Button to Buy Dash with cash
+- (IBAction)buyPIVXWithCashAction:(id)sender {
+    [sender setEnabled:NO];
+    
+    [self dismissViewControllerAnimated:NO completion:^{
+        NSString *token = [[NSUserDefaults standardUserDefaults] valueForKey:USER_DEFAULTS_AUTH_TOKEN];
+        if (token != nil && [token isEqualToString:@"(null)"] == FALSE) {
+            [self getOrders];
+        }
+        else {
+            [self pushToStep1];
+        }
+        [sender setEnabled:YES];
+    }];
+}
+
+-(void)pushToStep1
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:STORYBOARD_DASH bundle:nil];
+        UINavigationController *navController = (UINavigationController*) [storyboard instantiateViewControllerWithIdentifier:@"wocNavigationController"];
+        [navController.navigationBar setTintColor:[UIColor whiteColor]];
+        BRAppDelegate *appDelegate = (BRAppDelegate*)[[UIApplication sharedApplication] delegate];
+        appDelegate.window.rootViewController = navController;
+    });
+}
+
+// MARK: - WallofCoins API
+
+- (void)getOrders
+{
+    MBProgressHUD *hud  = [MBProgressHUD showHUDAddedTo:self.navigationController.topViewController.view animated:YES];
+    NSDictionary *params = @{
+                             //@"publisherId": @WALLOFCOINS_PUBLISHER_ID
+                             };
+    
+    [[APIManager sharedInstance] getOrders:nil response:^(id responseDict, NSError *error) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [hud hideAnimated:YES];
+        });
+        
+        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:STORYBOARD_DASH bundle:nil];
+        UINavigationController *navController = (UINavigationController*) [storyboard instantiateViewControllerWithIdentifier:@"wocNavigationController"];
+        
+        if (error == nil) {
+            if ([responseDict isKindOfClass:[NSArray class]])
+            {
+                NSArray *orders = [[NSArray alloc] initWithArray:(NSArray*)responseDict];
+                if (orders.count > 0) {
+                    NSString *phoneNo = [[NSUserDefaults standardUserDefaults] valueForKey:USER_DEFAULTS_LOCAL_PHONE_NUMBER];
+                    NSPredicate *wdvPredicate = [NSPredicate predicateWithFormat:@"status == 'WD'"];
+                    NSArray *wdArray = [orders filteredArrayUsingPredicate:wdvPredicate];
+                    
+                    if (wdArray.count > 0) {
+                        NSDictionary *orderDict = (NSDictionary*)[wdArray objectAtIndex:0];
+                        NSString *status = [NSString stringWithFormat:@"%@",[orderDict valueForKey:@"status"]];
+                        if ([status isEqualToString:@"WD"]) {
+                            WOCBuyingInstructionsViewController *myViewController = [storyboard instantiateViewControllerWithIdentifier:@"WOCBuyingInstructionsViewController"];
+                            myViewController.phoneNo = phoneNo;
+                            myViewController.isFromSend = YES;
+                            myViewController.isFromOffer = NO;
+                            myViewController.orderDict = orderDict;
+                            
+                            [navController pushViewController:myViewController animated:YES];
+                        }
+                    }
+                    else {
+                        
+                        WOCBuyingSummaryViewController *myViewController = [storyboard instantiateViewControllerWithIdentifier:@"WOCBuyingSummaryViewController"];
+                        myViewController.phoneNo = phoneNo;
+                        myViewController.orders = orders;
+                        myViewController.isFromSend = YES;
+                        
+                        [navController pushViewController:myViewController animated:YES];
+                    }
+                }
+            }
+        }
+        BRAppDelegate *appDelegate = (BRAppDelegate*)[[UIApplication sharedApplication] delegate];
+        appDelegate.window.rootViewController = navController;
+    }];
 }
 
 @end
